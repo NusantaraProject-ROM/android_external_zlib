fc5cb121eb0390d95bdfa0aacdaccaf90df49e53
Juhyung Park <qkrwngud825@gmail.com>
2022-05-23 01:16:10 +0900

    zlib: cpu_features: always enable ARMV8_ALWAYS_NEW
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: I02613131041a63300180b0bcaf1056a4beeb7eb9

--- cpu_features.h
+++ cpu_features.h
@@ -13,13 +13,21 @@ extern int x86_cpu_enable_sse2;
 extern int x86_cpu_enable_ssse3;
 extern int x86_cpu_enable_simd;
 
-#if defined(ARMV8_OS_MACOS)
-/* Crypto extensions (crc32/pmull) are a baseline feature in ARMv8.1-A, and
- * OSX running on arm64 is new enough that these can be assumed without
- * runtime detection.
+/*
+ * We safely assume every device that we support supports crc32/pmull hw
+ * features. Practically speaking, we never supported devices without it.
+ *
+ * Hence, simply enable ARMV8_ALWAYS_NEW to avoid the cost of evaluating
+ * it during runtime.
+ *
+ * In the 0% chance of us adding support for devices without these hw
+ * features, it'll be very apparent during bring-up and not cause a
+ * silent breakage.
+ *
+ * If you encounter build breakage with this change, you should review
+ * your compiler flag and see if it correctly enables crc hw feature.
  */
 #define ARMV8_ALWAYS_NEW
-#endif
 
 #if defined(ARMV8_ALWAYS_NEW)
 #define arm_cpu_enable_crc32 1
